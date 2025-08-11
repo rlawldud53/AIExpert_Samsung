ac2a0b50567024823f4125c95dd6347ea671f38f
Jeeyoung Kim <68314321+rlawldud53@users.noreply.github.com>
2025-08-11 16:03:18 +0900

    Update download_dataset.py

--- ai_expert/download_dataset.py
+++ ai_expert/download_dataset.py
@@ -4,6 +4,7 @@ from subprocess import run, CalledProcessError
 import re
 import gdown
 import json
+from tqdm.auto import tqdm
 
 def gdown_from_view_url(url: str, out: Path, show_progress: bool = True):
     m = re.search(r"/d/([a-zA-Z0-9_-]+)/", url)
